3dfea20eb32b0eea35695be2290d6c7685abc53f
fosujohn <fosujohn02@gmail.com>
2022-11-14 16:49:59 +0000

    desktop the rest

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ const gameDiv = document.querySelector('.game');
 const resultsDiv = document.querySelector('.results');
 const resultDivs = document.querySelectorAll('.results__result');
 
+const resultwinner = document.querySelector('.results__winner');
+const resultText = document.querySelector('.results__text');
+const playAgainBtn = document.querySelector('.play-again')
+
 // Game Logic
 choiceButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -39,7 +43,8 @@ choiceButtons.forEach(button => {
 
 function choose(choice) {
     const aichoice = aiChoose()
-    displayResults([choice, aichoice])
+    displayResults([choice, aichoice]);
+    displayWinner([choice, aichoice]);
 }
 
 function aiChoose() {
@@ -63,6 +68,44 @@ function displayResults(results) {
     resultsDiv.classList.toggle('hidden')
 }
 
+function displayWinner(results) {
+    setTimeout(() => {
+        const userWins = isWinner(results)
+        const aiWins = isWinner(results.reverse())
+
+        if(userWins) {
+            resultText.innerText = "you win"
+            resultDivs[0].classList.toggle('winner')
+        } else if(aiWins) {
+            resultText.innerText = "you lose"
+            resultDivs[1].classList.toggle('winner')
+        } else {
+            resultText.innerText = "draw"
+        }
+        resultwinner.classList.toggle('hidden')
+        resultsDiv.classList.toggle('show-winner')
+    }, 1000);
+
+    
+}
+
+function isWinner(results) {
+    return results[0].beats === results[1].name;
+}
+
+// Play Again
+playAgainBtn.addEventListener('click', () => {
+    gameDiv.classList.toggle('hidden')
+    resultsDiv.classList.toggle('hidden')
+
+    resultDivs.forEach(resultDiv => {
+        resultDiv.innerHTML = ""
+        resultDiv.classList.remove('winner')
+    })
+    resultText.innerText = ""
+    resultwinner.classList.toggle('hidden')
+    resultsDiv.classList.toggle('show-winner')
+})
 
 // Show/Hide Rules
 btnRules.addEventListener('click', () => {
